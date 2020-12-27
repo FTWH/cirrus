04c9a370205ff1e53513a9b91736a36dfbbb4745
FTWH <71968506+FTWH@users.noreply.github.com>
2020-12-27 23:52:50 +0800

    Update get_eigen.sh

--- third_party/get_eigen.sh
+++ third_party/get_eigen.sh
@@ -1,4 +1,4 @@
 wget https://gitlab.com/libeigen/eigen/-/archive/3.3.3/eigen-3.3.3.tar.bz2
 tar -xof eigen-3.3.3.tar.bz2
-mv eigen* eigen_source
+mv eigen-3.3.3 eigen_source
 rm -f eigen-3.3.3.tar.bz2
